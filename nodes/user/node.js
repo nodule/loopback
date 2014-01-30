e441a7a1b322f3466bdf4e0edb4f11561f74ad51
Rob Halff <rob.halff@gmail.com>
2014-01-30 23:05:52 +0100

    beautify

--- nodes/user/node.js
+++ nodes/user/node.js
@@ -1,4 +1,6 @@
 var User = input.loopback.User.extend('user');
 User.attachTo(input.user_connector);
 User.session.attachTo(input.session_connector);
-output = { user: User }
+output = {
+  user: User
+}
